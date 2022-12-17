ff94c55b3850244244a7e2884f190b8469ab111f
DeFranS325 <sansei325@gmail.com>
2022-12-17 13:36:00 +0400

    feat: add header and footer templates with css

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import './index.css';
+import headerTemplate from './pages/header/header';
+import footerTemplate from './pages/footer/footer';
 
-console.log('hey');
+const header: HTMLElement = document.getElementById('header') as HTMLElement;
+//const main: HTMLElement = document.getElementById('main') as HTMLElement;
+const footer: HTMLElement = document.getElementById('footer') as HTMLElement;
+
+header.innerHTML = headerTemplate();
+footer.innerHTML = footerTemplate();
---
+++ src/pages/footer/footer.ts
@@ -0,0 +1,22 @@
+export default function Footer() {
+    return `
+        <div class="footer-wrapper wrapper">
+            <div class="footer__author">
+                <a class="footer__author_github_link" href="https://github.com/DeFranS325">
+                    <img class="footer__author_github_img" src="./assets/svg/github.svg" />
+                    <span class="footer__author_github_name">DeFranS325</span>
+                </a>
+                <a class="footer__author_github_link" href="https://github.com/sohodoll">
+                    <img class="footer__author_github_img" src="./assets/svg/github.svg" />
+                    <span class="footer__author_github_name">Sohodoll</span>
+                </a>
+            </div>
+            <div class="footer__site-year">2022</div>
+            <div class="footer__rsshool">
+                <a class="footer__rsshool_link" href="https://rs.school/js/">
+                    <img class="footer__rsshool_img" src="./assets/svg/rs_school.svg" />
+                </a>
+            </div>
+        </div>
+    `;
+}
