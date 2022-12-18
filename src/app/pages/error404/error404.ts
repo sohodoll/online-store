3f4e207ee9b7cf9483db363deed40022951fc93f
DeFranS325 <sansei325@gmail.com>
2022-12-18 18:55:18 +0400

    feat: add folder ./pages/cart and ./pages/error404,and add files app.ts, item.ts

---
+++ src/app/pages/error404/error404.ts
@@ -0,0 +1,7 @@
+export default function Error404(): string {
+    return `
+        <div class="error404-wrapper wrapper">
+            <img class="error404__img" src="../../../assets/gif/error404.gif" alt="404"/>
+        </div>
+    `;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import './index.css';
-import headerTemplate from './pages/header/header';
-import footerTemplate from './pages/footer/footer';
+import App from './app/app';
 
-const header: HTMLElement = document.getElementById('header') as HTMLElement;
-//const main: HTMLElement = document.getElementById('main') as HTMLElement;
-const footer: HTMLElement = document.getElementById('footer') as HTMLElement;
-
-header.innerHTML = headerTemplate();
-footer.innerHTML = footerTemplate();
+const app = new App();
+app.start();
