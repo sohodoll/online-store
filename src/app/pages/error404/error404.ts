00875b99ead3960b02e06dad353107ac118da298
sohodoll <kovalevvtl@gmail.com>
2022-12-22 14:04:40 +0300

    fix: delete app folder and fix paths

--- src/app/pages/error404/error404.ts
+++
@@ -1,7 +0,0 @@
-export default function Error404(): string {
-    return `
-        <div class="error404-wrapper wrapper">
-            <img class="error404__img" src="../../../assets/gif/error404.gif" alt="404"/>
-        </div>
-    `;
-}
