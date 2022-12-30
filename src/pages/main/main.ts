d0990317091fc636d43272f0f6550bf4a78dc15d
sohodoll <kovalevvtl@gmail.com>
2022-12-30 13:37:57 +0300

    feat: add asics filter

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -28,31 +28,55 @@ class MainPage extends Page {
     private createFilters(): void {
         const brandFilter = <HTMLDivElement>document.createElement('div');
         const adidasFilter = <HTMLDivElement>document.createElement('div');
+        const asicsFilter = <HTMLDivElement>document.createElement('div');
         const main = <HTMLElement>document.querySelector('main');
         adidasFilter.innerText = 'Adidas';
+        asicsFilter.innerText = 'Asics';
         adidasFilter.classList.add('brand');
+        asicsFilter.classList.add('brand');
+        asicsFilter.id = 'asics-filter';
         adidasFilter.id = 'adidas-filter';
         brandFilter.classList.add('brand__filter');
         brandFilter.appendChild(adidasFilter);
+        brandFilter.appendChild(asicsFilter);
         main.prepend(brandFilter);
         this.listen();
     }
 
     private listen() {
-        const adidasButton = <HTMLDivElement>document.querySelector('.brand');
-        const brand = adidasButton.innerText;
-        adidasButton.addEventListener('click', (e) => {
-            adidasButton.classList.toggle('brand_active');
-            if (adidasButton.classList.contains('brand_active')) {
-                setSearchParams(brand);
-                const filteredArray = filterItems(shoes, brand);
-                this.createListItem(filteredArray);
-            } else {
-                removeSearchParams();
-                this.createListItem(shoes);
-            }
-            viewButtonAddClick();
+        const adidasButton = <HTMLDivElement>document.querySelector('#adidas-filter');
+        const asicsButton = <HTMLDivElement>document.querySelector('#asics-filter');
+        const buttons: HTMLDivElement[] = [];
+        buttons.push(adidasButton, asicsButton);
+
+        buttons.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                const brand = button.innerText;
+                button.classList.toggle('brand_active');
+                if (button.classList.contains('brand_active')) {
+                    setSearchParams(brand);
+                    const filteredArray = filterItems(shoes, brand);
+                    this.createListItem(filteredArray);
+                } else {
+                    removeSearchParams();
+                    this.createListItem(shoes);
+                }
+                viewButtonAddClick();
+            });
         });
+        // const brand = adidasButton.innerText;
+        // adidasButton.addEventListener('click', (e) => {
+        //     adidasButton.classList.toggle('brand_active');
+        //     if (adidasButton.classList.contains('brand_active')) {
+        //         setSearchParams(brand);
+        //         const filteredArray = filterItems(shoes, brand);
+        //         this.createListItem(filteredArray);
+        //     } else {
+        //         removeSearchParams();
+        //         this.createListItem(shoes);
+        //     }
+        //     viewButtonAddClick();
+        // });
     }
 
     private handleQueryStorage(params: URLSearchParams) {
@@ -60,7 +84,20 @@ class MainPage extends Page {
         setSearchParams(brand);
         const filteredArray = filterItems(shoes, brand);
         this.createListItem(filteredArray);
-        console.log(brand, filteredArray);
+
+        setTimeout(() => {
+            if (brand) {
+                const adidasButton = <HTMLDivElement>document.querySelector('#adidas-filter');
+                const asicsButton = <HTMLDivElement>document.querySelector('#asics-filter');
+                const buttons: HTMLDivElement[] = [];
+                buttons.push(adidasButton, asicsButton);
+                buttons.forEach((button) => {
+                    if (brand === button.innerText) {
+                        button.classList.add('brand_active');
+                    }
+                });
+            }
+        }, 100);
     }
 
     render() {
