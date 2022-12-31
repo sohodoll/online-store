2c36adaba25b04c48f852a0b3946696554881a28
sohodoll <kovalevvtl@gmail.com>
2022-12-31 11:54:38 +0300

    feat: update filters counts

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -3,7 +3,7 @@ import { createCartItemFromMain, IPrototypeItem } from '../templates/items';
 //import ItemCart from '../components/itemCart/itemCart';
 import shoes from '../../db/shoes';
 import { setSearchParams, removeSearchParams, filterItems } from '../templates/filters';
-import { viewButtonAddClick } from '../../app';
+import { updateHeader, viewButtonAddClick } from '../../app';
 
 let selectBrand: HTMLDivElement | undefined;
 let selectCategory: HTMLDivElement | undefined;
@@ -50,16 +50,138 @@ class MainPage extends Page {
         main.prepend(filterElement);
     }
 
+    updateCount() {
+        const brandFiltersList: NodeListOf<HTMLDivElement> = document.querySelectorAll('.brand');
+        const brandFilters = Array.from(brandFiltersList);
+        const catFiltersList: NodeListOf<HTMLDivElement> = document.querySelectorAll('.category');
+        const catFilters = Array.from(catFiltersList);
+        if (selectBrand) {
+            brandFilters.forEach((brand) => {
+                if (selectBrand !== brand) {
+                    brand.children[0].textContent = '0/';
+                } else {
+                    const innerArray = filterItems(
+                        shoes,
+                        brand.id.substring(0, brand.id.indexOf('-')).charAt(0).toUpperCase() +
+                            brand.id.substring(0, brand.id.indexOf('-')).slice(1),
+                        catFilter
+                    );
+                    brand.children[0].textContent = `${innerArray.length}/`;
+                }
+            });
+        } else if (selectCategory) {
+            catFilters.forEach((category) => {
+                if (selectCategory !== category) {
+                    category.children[0].textContent = '0/';
+                } else {
+                    const innerArray = filterItems(
+                        shoes,
+                        brandFilter,
+                        category.id.substring(0, category.id.indexOf('-')).charAt(0).toUpperCase() +
+                            category.id.substring(0, category.id.indexOf('-')).slice(1)
+                    );
+                    category.children[0].textContent = `${innerArray.length}/`;
+                }
+            });
+        } else {
+            brandFilters.forEach((brand) => {
+                if (selectBrand !== brand) {
+                    const innerArray = filterItems(
+                        shoes,
+                        brand.id.substring(0, brand.id.indexOf('-')).charAt(0).toUpperCase() +
+                            brand.id.substring(0, brand.id.indexOf('-')).slice(1),
+                        catFilter
+                    );
+                    brand.children[0].textContent = `${innerArray.length}/`;
+                }
+            });
+            catFilters.forEach((category) => {
+                if (selectCategory !== category) {
+                    const innerArray = filterItems(
+                        shoes,
+                        brandFilter,
+                        category.id.substring(0, category.id.indexOf('-')).charAt(0).toUpperCase() +
+                            category.id.substring(0, category.id.indexOf('-')).slice(1)
+                    );
+                    category.children[0].textContent = `${innerArray.length}/`;
+                }
+            });
+        }
+    }
+
+    // updateBrandCount(selectedFilter: string) {
+    //     const brandFiltersList: NodeListOf<HTMLDivElement> = document.querySelectorAll('.brand');
+    //     const brandFilters = Array.from(brandFiltersList);
+    //     const filteredArray = filterItems(shoes, selectedFilter, '');
+    //     brandFilters.forEach((brand) => {
+    //         const idToUpper =
+    // brand.id.substring(0, brand.id.indexOf('-')).charAt(0).toUpperCase() +
+    // brand.id.substring(0, brand.id.indexOf('-')).slice(1);
+    //         if (!selectBrand) {
+    //             const filteredArray = filterItems(shoes, idToUpper, catFilter);
+    //             brand.children[0].textContent = `${filteredArray.length}/`;
+    //         } else {
+    // if (brand.id.substring(0, brand.id.indexOf('-')) !== selectedFilter.toLowerCase()) {
+    //                 const filteredArray: [] = [];
+    //                 brand.children[0].textContent = `${filteredArray.length}/`;
+    //             } else {
+    //                 this.updateCategoryCount(idToUpper);
+    //                 brand.children[0].textContent = `${tempArray.length}/`;
+    //             }
+    //         }
+    //     });
+    // }
+
+    // updateCategoryCount(brand: string) {
+    // const catFiltersList: NodeListOf<HTMLDivElement> = document.querySelectorAll('.category');
+    // const catFilters = Array.from(catFiltersList);
+
+    //     catFilters.forEach((category) => {
+    //         if (selectBrand) {
+    //             const idToUpper =
+    //                 category.id.substring(0, category.id.indexOf('-')).charAt(0).toUpperCase() +
+    //                 category.id.substring(0, category.id.indexOf('-')).slice(1);
+    //             console.log(idToUpper);
+    //             const filteredArray = filterItems(shoes, brand, idToUpper);
+    //             category.children[0].textContent = `${filteredArray.length}/`;
+    //         } else {
+    //             const filteredArray = filterItems(shoes, brandFilter, catFilter);
+    //             category.children[0].textContent = `${filteredArray.length}/`;
+    //         }
+    //     });
+    // }
+
     createFilterElements(array: string[], type: string): HTMLDivElement {
         const filterElement = <HTMLDivElement>document.createElement('div');
         array.forEach((element) => {
             const elementDiv = document.createElement('div');
+            const currCount = document.createElement('div');
+            const maxCount = document.createElement('div');
+            currCount.id = `${element}-count`;
+            if (type === 'brand') {
+                const filteredArray = filterItems(shoes, element, '');
+                currCount.textContent = `${filteredArray.length}/`;
+                maxCount.textContent = `${filteredArray.length}`;
+            } else if (type === 'category') {
+                const filteredArray = filterItems(shoes, '', element);
+                currCount.textContent = `${filteredArray.length}/`;
+                maxCount.textContent = `${filteredArray.length}`;
+            }
             elementDiv.className = type;
             elementDiv.id = `${element.toLowerCase()}-filter`;
             elementDiv.textContent = element;
+            elementDiv.append(currCount, maxCount);
 
             elementDiv.addEventListener('click', () => {
                 let filteredArray: IPrototypeItem[] = [];
+                // if (type === 'brand') {
+                //     filteredArray = filterItems(shoes, element, '');
+                //     elementDiv.textContent = `${element}(${filteredArray.length})`;
+                // }
+                // if (type === 'category') {
+                //     const filteredArray = filterItems(shoes, '', element);
+                //     elementDiv.textContent = `${element}(${filteredArray.length})`;
+                // }
 
                 if (type === 'brand') {
                     if (selectBrand) {
@@ -74,7 +196,6 @@ class MainPage extends Page {
                         } else {
                             brandFilter = '';
                             tempArray = filterItems(shoes, brandFilter, catFilter);
-
                             removeSearchParams([type]);
                             this.createListItem(tempArray);
                             elementDiv.classList.remove(`${type}_active`);
@@ -88,6 +209,7 @@ class MainPage extends Page {
                         tempArray = filterItems(shoes, brandFilter, catFilter);
                         this.createListItem(tempArray);
                     }
+                    this.updateCount();
                 } else {
                     if (selectCategory) {
                         if (selectCategory !== elementDiv) {
@@ -114,6 +236,7 @@ class MainPage extends Page {
                         tempArray = filterItems(shoes, brandFilter, catFilter);
                         this.createListItem(tempArray);
                     }
+                    this.updateCount();
                 }
 
                 viewButtonAddClick();
